380269bf77ba9128375f68fd72cce8edfde5ce3b
Marc Queudot <marc.queudot@viacesi.fr>
2022-04-19 00:38:56 -0400

    Install git v2

--- node.sh
+++ node.sh
@@ -1,1 +1,8 @@
+# Node
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+# Git v2
+yum install -y https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm \
+&& yum install -y git \
+&& yum -y clean all \
+&& rm -rf /var/cache
